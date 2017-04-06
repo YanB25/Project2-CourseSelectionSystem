c08faf8f36d97e922cab5fa9b9aeb298e2e0e130
YanB25 <442826556@qq.con>
2017-04-06 08:18:44 +0800

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
 #include "Course.hpp"
-#include "Persons.hpp"
+#include "Person.hpp"
 #include "System.hpp"
+#include <string>
+#include <iostream>
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
 int main() {
+	Student s("yb", "16337269");
+	Teacher t("yyh", "16337270");
+	Course c("cs", "001"); 
+	c.addStudent(s);
+	cout << c.studentIsIn(s) << endl;
+	c.addTeacher(t);
+	cout << c.teacherIsIn(t) << endl;
+	cout << c.addStudent(s) << endl;
+	cout << c.removeStudent(s) << endl;
+	c.clearTeacher();
+	cout << c.teacherIsIn(t) << endl;
+	cout << c.removeTeacher(t) << endl;
 	return 0;
 }
