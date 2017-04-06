2b9533c2978220067c2a4dc841e2a3fbec74e0ef
YanB25 <442826556@qq.con>
2017-04-06 08:21:49 +0800

    main class of the system

--- src/System.hpp
+++ src/System.hpp
@@ -2,16 +2,36 @@
 #define _YB_SYSTEM_HPP_
 
 #include <set>
-#include "Persons.hpp"
+#include <string>
+#include "Person.hpp"
+#include "Course.hpp"
 using std::set;
+using std::string;
 
 class System {
 public:
-	//type for a group of students and teachers
-	typedef set<Student> stu_group_t;
-	typedef set<Teacher> tea_group_t;
+	typedef Group<Course> course_group_t;
+	typedef System self;
+	typedef string id_t;
 	
-	System() { };
+	//TODO:important:构造函数和析构函数需要完成文件IO的工作。
+	//TODO:start here
+	System();
+	System(const System&) = delete;
+	~System() = default;
+	
+	bool hasCourse(const Course& crs) const { return _course_group.isIn(crs); }
+	const Course& GetCourse(const id_t& crs_id) { return *_course_group.find(Course("Unknown", crs_id)); }
+	
+	bool joinStuToCourse(const Student& stu, const Course& crs);
+	bool joinTeaToCourse(const Teacher& tea, const Course& crs);
+	bool removeStuFromCourse(const Student& stu, const Course& crs);
+	bool removeTeaFromCourse(const Teacher& tea, const Course& crs);
+	bool teaIsInCourse(const Teacher& tea, const Course& crs);
+	bool stuIsInCourse(const Student& stu, const Course& crs);
 	//TODO:not finished
+	
+private:
+	course_group_t _course_group;
 };
 #endif
