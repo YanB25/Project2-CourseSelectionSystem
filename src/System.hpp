cdc0ca8f2d1403f6c38b69077cb2d75ef9f4cc1e
YanB25 <442826556@qq.con>
2017-04-06 09:35:19 +0800

    imple System-class

--- src/System.hpp
+++ src/System.hpp
@@ -16,19 +16,22 @@ public:
 	
 	//TODO:important:构造函数和析构函数需要完成文件IO的工作。
 	//TODO:start here
-	System();
+	System() = default;
 	System(const System&) = delete;
 	~System() = default;
 	
 	bool hasCourse(const Course& crs) const { return _course_group.isIn(crs); }
-	const Course& GetCourse(const id_t& crs_id) { return *_course_group.find(Course("Unknown", crs_id)); }
+	Course& GetCourse(const id_t& crs_id) { return const_cast<Course&>(*_course_group.find(Course("Unknown", crs_id))); }
 	
-	bool joinStuToCourse(const Student& stu, const Course& crs);
-	bool joinTeaToCourse(const Teacher& tea, const Course& crs);
-	bool removeStuFromCourse(const Student& stu, const Course& crs);
-	bool removeTeaFromCourse(const Teacher& tea, const Course& crs);
-	bool teaIsInCourse(const Teacher& tea, const Course& crs);
-	bool stuIsInCourse(const Student& stu, const Course& crs);
+	bool joinStuToCourse(const Student& stu, Course& crs) { return _course_group.isIn(crs) && crs.addStudent(stu); }
+	bool joinTeaToCourse(const Teacher& tea, Course& crs) { return _course_group.isIn(crs) && crs.addTeacher(tea); }
+	bool removeStuFromCourse(const Student& stu, Course& crs) { return _course_group.isIn(crs) && crs.removeStudent(stu); }
+	bool removeTeaFromCourse(const Teacher& tea, Course& crs) { return _course_group.isIn(crs) && crs.removeTeacher(tea); }
+	bool teaIsInCourse(const Teacher& tea, const Course& crs) { return crs.teacherIsIn(tea); }
+	bool stuIsInCourse(const Student& stu, const Course& crs) { return crs.studentIsIn(stu); }
+	
+	bool addCourse(const Course& crs) { return _course_group.insert(crs); }
+	bool removeCourse(const Course& crs) { return _course_group.erase(crs); }
 	//TODO:not finished
 	
 private:
